7fb553d42dcb43cfdf5673c6a1ee23f6688ece57
Aswin Gunavelu Mohan <agunavel@turing-2.eng.asu.edu>
2016-11-12 00:28:41 -0700

    making more readable

--- run.py
+++ run.py
@@ -17,13 +17,22 @@ docTFIDFs = json.load(open("stack-tfidf.json"))
 
 while(1):
 	query = raw_input("Please enter question: ")
+	print("\n")
 	maxSimilarity = -1
 	bestQuestion = ""
-	for question in questions:
-		similarity = utils.cosineSimilarity(utils.getTFIDF(query, questions),docTFIDFs[question])
+	print("-----------------------")
+	print("Calculating tfidf for the query with all questions as reference ...")
+	queryTFIDF = utils.getTFIDF(query, questions)
+	print("Calling cosine similarity between all the questions to find best match ...")
+	for i in range(len(questions)):
+		question = questions[i]
+		similarity = utils.cosineSimilarity(queryTFIDF,docTFIDFs[question])
 		if similarity > maxSimilarity:
+			print(similarity)
 			maxSimilarity = similarity
 			bestQuestion = question
-	print("best answer : " + data[bestQuestion])
+	print("Best question match : " + bestQuestion)
+	print("Max similarity score : " + str(maxSimilarity))
+	print("Best answer : " + data[bestQuestion])
 
 
